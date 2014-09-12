c5a640ad7132620e23d4c026e243040717a33642
Lucas Lain <lucas.lain@gmail.com>
2014-09-12 16:02:44 -0400

    .

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
-var scrollMinX = 0;
-var scrollMinY = 0;
-var scrollMaxX = document.body.scrollWidth - window.innerWidth;
-var scrollMaxY = document.body.scrollHeight - window.innerHeight;
+var scroll = function(e) {
+    // compute state
+    if (stopScrollX || stopScrollY) {
+        e.preventDefault();              // this one is the key
+        e.stopPropagation();
+        window.scroll(scrollToX, scrollToY);
+    }
+}
 
-// make sure that we work with the correct dimensions
-window.onresize = function () {
-    scrollMaxX = document.body.scrollWidth - window.innerWidth;
-    scrollMaxY = document.body.scrollHeight - window.innerHeight;
-};
-
-// where the magic happens
-document.onscroll = function () {    
-    if (window.scrollX <= scrollMinX) scrollTo(scrollMinX, window.scrollY);
-    if (window.scrollX >= scrollMaxX) scrollTo(scrollMaxX, window.scrollY);
-
-    if (window.scrollY <= scrollMinY) scrollTo(window.scrollX, scrollMinY);
-    if (window.scrollY >= scrollMaxY) scrollTo(window.scrollX, scrollMaxY);
-};
+document.addEventListener('mousewheel', scroll, false);;
